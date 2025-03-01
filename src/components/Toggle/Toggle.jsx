474ae29c35811f64c1d9ce9602f085e3220df12b
Jessica <jessicamendez328@gmail.com>
2025-03-01 14:31:14 -0500

    create toggle

---
+++ src/components/Toggle/Toggle.jsx
@@ -0,0 +1,12 @@
+import './Toggle.css';
+
+export default function Toggle() {
+  return (
+    // <div className="Toggle">
+<label class="switch">
+  <input type="checkbox" ></input>
+  <span class="slider round"></span>
+</label>
+// </div>
+  );
+}
