ecf41cf02a105cdf716ae05cf4913036e08036f1
Jessica <jessicamendez328@gmail.com>
2025-03-12 22:22:44 -0400

    nav horizontal, hover state

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react';
 import './App.css';
-import Header from './components/Header/Header';
-import Nav from './components/Nav/Nav';
+import Header from '../../components/Header/Header';
+import Nav from '../../components/Nav/Nav';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      <Nav />
       <main className="App-body">
       </main>
     </div>
