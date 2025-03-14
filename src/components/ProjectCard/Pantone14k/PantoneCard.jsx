20b3516377af4a455909267fb9048d50ca125387
Jessica <jessicamendez328@gmail.com>
2025-03-13 20:50:27 -0400

    dynamically render pintern logo

--- src/components/ProjectCard/Pantone14k/PantoneCard.jsx
+++ src/components/ProjectCard/Pantone14k/PantoneCard.jsx
@@ -1,11 +1,21 @@
 import './PantoneCard.css';
 import { Link } from 'react-router-dom';
+import pantonePendant from '../../../../public/assets/images/project-images/pantone-pendant/pantone-pendant.png';
 
 export default function PantoneCard() {
 
   return ( 
     <main className="Project-Card">
-      <h2>Pantone 14k</h2>
+      <div className="card-title">
+        <Link to="/projects/pantone14k">
+        <h2>Pantone 14k</h2>
+      </Link>
+      </div>
+      <div className="card-image">
+      <Link to="/projects/pantone14k">
+      <img src={pantonePendant} alt="Pantone Swatch Necklace Pendant in 14k Gold" />
+      </Link>
+      </div>
       </main>
   );
 }
