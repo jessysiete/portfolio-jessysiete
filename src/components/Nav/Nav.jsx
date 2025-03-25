f75a97704b8a2c151727dd6504e92c566690aa50
Jessica <jessicamendez328@gmail.com>
2025-03-24 20:52:54 -0400

    added local storage to store nav location & styling

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,46 +1,43 @@
 import './Nav.css';
 import Pdf from '../../assets/Documents/resume.pdf';
-import { Link } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 
 export default function Nav() {
-
-  const handleLinkClick = (event) => {
-    const links = document.querySelectorAll('.menu-link');
-    links.forEach(link => link.classList.remove('active'));
-    event.currentTarget.classList.add('active');
-  };
-
   const location = useLocation();
-  useEffect(() => {
-    if (location.pathname === '/') {
-      const links = document.querySelectorAll('.menu-link');
-      links.forEach(link => link.classList.remove('active'));
-    }
-  }, [location]);
 
+  useEffect(() => {
+    // Store the current path in localStorage to persist active state
+    localStorage.setItem("activeNav", location.pathname);
+  }, [location]);
 
   return (
     <nav className="menu">
       <ul className="menu-list">
         <li className="menu-item-projects">
-          <Link to="/projects" className="menu-link" onClick={handleLinkClick}>Projects</Link>
+          <Link 
+            to="/projects" 
+            className={`menu-link ${location.pathname === "/projects" ? "active" : ""}`}
+          >
+            Projects
+          </Link>
         </li>
         <li className="menu-item-github">
-          <Link to="/github" className="menu-link" onClick={handleLinkClick}>GitHub</Link>
-          {/* <a href="https://github.com/siguejessy" target="blank" className="menu-link" onClick={handleLinkClick}>Github</a> */}
+          <Link 
+            to="/github" 
+            className={`menu-link ${location.pathname === "/github" ? "active" : ""}`}
+          >
+            GitHub
+          </Link>
         </li>
         <li className="menu-item-resume">
-          <Link to="/resume" className="menu-link" onClick={handleLinkClick}>Resume</Link>
-          {/* <a href={Pdf} target="blank" className="menu-link" onClick={handleLinkClick}>Resume</a> */}
+          <Link 
+            to="/resume" 
+            className={`menu-link ${location.pathname === "/resume" ? "active" : ""}`}
+          >
+            Resume
+          </Link>
         </li>
-        {/* <li className="menu-item">
-          <a href="https://www.linkedin.com/in/siguejessy" target="blank" className="menu-link" onClick={handleLinkClick}>LinkedIn</a>
-        </li> */}
-        {/* <li className="menu-item">
-          <a href="mailto:jessica@jessysiete.com?subject=Greetings" className="menu-link" onClick={handleLinkClick}>Email</a>
-        </li> */}
       </ul>
     </nav>
   );
