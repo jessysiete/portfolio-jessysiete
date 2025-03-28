928153017b6b8b14d43dcc3c95a7e7c313346a82
Jessica <jessicamendez328@gmail.com>
2025-03-27 20:11:55 -0400

    resume page remove blurb and adjust layout

--- src/pages/Resume/ResumePage.jsx
+++ src/pages/Resume/ResumePage.jsx
@@ -1,39 +1,31 @@
-import { Link } from 'react-router-dom';
-import Pdf from '../../assets/Documents/resume.pdf';
+import { Link } from "react-router-dom";
+import Pdf from "../../assets/Documents/resume.pdf";
 
-
-import './ResumePage.css';
+import "./ResumePage.css";
 
 export default function ResumePage() {
   return (
     <>
-    <div className="resume-page">
-    <div className="page-marker-resume"></div>
-      <h1 className="intro">
-        Work Experience
-      </h1>
-      <p className="intro">
-        Welcome to my portfolio. I am a full stack web developer with a passion for creating beautiful and functional web applications. I have experience with HTML, CSS, JavaScript, React, Node, Express, and MongoDB. I am always looking for new opportunities to learn and grow as a developer.
-      </p>
-
+        <div className="page-marker-resume"></div>
+      <div className="resume-page">
         <button className="view-resume">
-          <a href={Pdf} target="blank" className="menu-link">View PDF Resume</a>
+          <a href={Pdf} target="blank" className="button-link">
+            View PDF Resume
+          </a>
         </button>
 
-
-
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
       </div>
-      </>
+    </>
   );
 }
